98db565650d0b8e3714ebabf8358de9b2637bd47
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-10-25 15:26:25 +0330

    refactor(notifier-api): Update start-command to add user to category members list

--- packages/notifier-api/src/command/start-command.ts
+++ packages/notifier-api/src/command/start-command.ts
@@ -1,47 +1,69 @@
+import {config, logger} from '../config.js';
 import {bot} from '../lib/bot.js';
-import {config, logger} from '../lib/config.js';
-import {message} from '../lib/i18n.js';
+import {message} from '../lib/message.js';
 import {alwatrNitrobase} from '../lib/nitrobase.js';
 
+import type {Category} from '../type.js';
 
-/**
- * `start` command.
- *
- * This command starts the bot.
- */
-bot.command('start', async (ctx) => {
-  const chatId = ctx.chat.id + '';
-  const categoryId = ctx.match;
-  logger.logMethodArgs?.('start-command', {chatId, categoryId});
+bot.command(
+  'start',
 
-  if (!categoryId) {
-    await ctx.reply(message('itIsPrivateBot'), {
-      reply_parameters: {message_id: ctx.message!.message_id},
-    });
+  /**
+   * Start command with category id as parameter.
+   * Adds the user to the category members list.
+   *
+   * @example
+   * `/start categoryId`
+   * `https://t.me/your_bot?start=categoryId`
+   */
+  async function startCommand(ctx) {
+    const categoryId = ctx.match;
+    try {
+      logger.logMethodArgs?.('startCommand', {categoryId, chat: ctx.chat});
 
-    return;
+      if (!categoryId) {
+        logger.incident?.('startCommand', 'category_id_not_provided', {from: ctx.from, chat: ctx.chat});
+        await ctx.reply(message.private_bot_warning, {
+          reply_parameters: {
+            message_id: ctx.msg.message_id,
+          },
+        });
+        return;
+      }
+
+      const categoriesCollection = await alwatrNitrobase.openCollection<Category>(config.nitrobase.categoriesCollection);
+
+      if (categoriesCollection.hasItem(categoryId) === false) {
+        logger.incident?.('startCommand', 'category_not_found', {categoryId, from: ctx.from, chat: ctx.chat});
+        await ctx.reply(message.invalid_data_submitted, {
+          reply_parameters: {
+            message_id: ctx.msg.message_id,
+          },
+        });
+        return;
+      }
+
+      const members = categoriesCollection.getItemData(categoryId).members;
+
+      if (members.findIndex((member) => member.id === ctx.chat.id) !== -1) {
+        await ctx.reply(message.already_added_to_list);
+        return;
+      }
+
+      members.push({
+        id: ctx.chat.id,
+        type: ctx.chat.type,
+        title: ctx.chat.title,
+        firstName: ctx.chat.first_name,
+        lastName: ctx.chat.last_name,
+        username: ctx.chat.username,
+      });
+      categoriesCollection.save();
+
+      await ctx.reply(message.success_added_to_list);
+    }
+    catch (error) {
+      logger.error?.('startCommand', 'unexpected_error', error, {categoryId, from: ctx.from, chat: ctx.chat});
+    }
   }
-
-  const categoriesCollection = await alwatrNitrobase.openCollection<CategoryItem>(config.nitrobase.categoriesCollection);
-
-  const newMember = {
-    id: chatId,
-    type: ctx.chat.type,
-    first_name: ctx.chat.first_name,
-    last_name: ctx.chat.last_name,
-    username: ctx.chat.username
-  }
-
-  const categoryData = categoriesCollection.getItemData(categoryId);
-  const targetIndex = categoryData.memberList.findIndex(member => member.id === chatId);
-  if (targetIndex === -1) {
-    categoryData.memberList.push(newMember);
-    categoriesCollection.mergeItemData(categoryId, categoryData);
-    categoriesCollection.save();
-
-    await ctx.reply(message('addedToList'));
-    return;
-  }
-
-  await ctx.reply(message('hasBeenAddedAlready'));
-});
+);
