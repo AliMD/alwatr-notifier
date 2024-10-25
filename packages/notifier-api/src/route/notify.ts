cead5b121c7715bca14075407651c460ace0e46a
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-10-25 11:39:55 +0330

    refactor(notifier-api): rewrite notify route

--- packages/notifier-api/src/route/notify.ts
+++ packages/notifier-api/src/route/notify.ts
@@ -1,21 +1,80 @@
-import {sendMessageToCategory} from '../bot/send-message.js';
-import {logger} from '../lib/config.js';
-import {parseBodyAsJson} from '../lib/pre-handlers/parse-request-body.js';
-import {validateCategoryId} from '../lib/pre-handlers/validate-category-id.js';
+import {HttpStatusCodes} from 'alwatr/nanotron';
+
+import {config, logger} from '../config.js';
+import {parseBodyAsJson} from '../handler/parse-body-as-json.js';
+import {bot} from '../lib/bot.js';
+import {alwatrNitrobase} from '../lib/nitrobase.js';
 import {nanotronApiServer} from '../lib/server.js';
 
-nanotronApiServer.defineRoute<{body: { categoryId: string; message: string }}>({
-  method: 'POST',
-  url: '/telegram/notify',
-  preHandlers: [parseBodyAsJson, validateCategoryId],
-  async handler() {
-    logger.logMethodArgs?.('defineRoute(/telegram/notify)', {body: this.sharedMeta.body});
+import type { GrammyError } from 'grammy';
 
-    sendMessageToCategory(this.sharedMeta.body.categoryId, this.sharedMeta.body.message);
+nanotronApiServer.defineRoute<{body: JsonObject}>({
+  method: 'POST',
+  url: 'notify',
+  preHandlers: [parseBodyAsJson],
+  async handler() {
+    logger.logMethod?.('notify');
+
+    const body = this.sharedMeta.body;
+
+    const categoryId = body.category;
+    if (categoryId === undefined || typeof categoryId !== 'string') {
+      this.serverResponse.statusCode = HttpStatusCodes.Error_Client_400_Bad_Request;
+      this.serverResponse.replyJson({
+        ok: false,
+        errorCode: 'category_required',
+        errorMessage: 'Category is required.',
+      });
+      return;
+    }
+
+    const message = body.message;
+    if (message === undefined || typeof message !== 'string') {
+      this.serverResponse.statusCode = HttpStatusCodes.Error_Client_400_Bad_Request;
+      this.serverResponse.replyJson({
+        ok: false,
+        errorCode: 'message_required',
+        errorMessage: 'Message is required.',
+      });
+      return;
+    }
+
+    const categoriesCollection = await alwatrNitrobase.openCollection<Category>(config.nitrobase.categoriesCollection);
+
+    if (categoriesCollection.hasItem(categoryId) === false) {
+      this.serverResponse.statusCode = HttpStatusCodes.Error_Client_400_Bad_Request;
+      this.serverResponse.replyJson({
+        ok: false,
+        errorCode: 'category_not_found',
+        errorMessage: 'Category not found.',
+      });
+      return;
+    }
 
     this.serverResponse.replyJson({
       ok: true,
-      data: {},
     });
+
+    const members = categoriesCollection.getItemData(categoryId).members;
+    for (let i = members.length - 1; i >= 0; i--) {
+      const member = members[i];
+      try {
+        await bot.api.sendMessage(member.id, message, {
+          parse_mode: body.markdown === true ? 'MarkdownV2' : undefined,
+        });
+      }
+      catch (error_) {
+        const error = error_ as GrammyError;
+        logger.error('sendMessage', 'error_send_message', error, {member, categoryId, message});
+
+        // Remove if blocked
+        if (error.error_code === 403) {
+          const memberIndex = members.findIndex((member_) => member_ === member);
+          members.splice(memberIndex, 1);
+          categoriesCollection.save();
+        }
+      }
+    }
   },
 });
+
