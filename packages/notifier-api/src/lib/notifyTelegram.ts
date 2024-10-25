f0a6847299f5305661dc4cc17ab036b2ea1b5864
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-10-25 15:27:24 +0330

    refactor(notifier-api): Update notifyTelegram function to send messages to all members of a category

--- packages/notifier-api/src/lib/notifyTelegram.ts
+++ packages/notifier-api/src/lib/notifyTelegram.ts
@@ -1,38 +1,38 @@
-import {bot} from './bot.js';
 import {config, logger} from '../config.js';
-import {escapeMessage} from './escape-message.js';
+import {bot} from './bot.js';
 import {alwatrNitrobase} from './nitrobase.js';
 
+import type {Category, NotifyOption} from '../type.js';
 import type {GrammyError} from 'grammy';
 
 /**
- * Send a message to members of `categoryId`.
- *
- * @param categoryId - Category(group) ID.
- * @param message_ - Message to send.
+ * Send a message to all members of the `categoryId`.
  */
-export async function
-sendMessageToCategory(categoryId: string, message_: string): Promise<void> {
-  logger.logMethodArgs?.('sendMessageToCategory', {categoryId});
-  message_ = escapeMessage(message_);
-
-  const categoriesCollection = await alwatrNitrobase.openCollection<CategoryItem>(config.nitrobase.categoriesCollection);
-  const categoryData = categoriesCollection.getItemData(categoryId);
-
-  for (const member of categoryData.memberList) {
+export async function notifyTelegram(option: NotifyOption): Promise<void> {
+  logger.logMethodArgs?.('notifyTelegram', option);
+  const categoriesCollection = await alwatrNitrobase.openCollection<Category>(config.nitrobase.categoriesCollection);
+  const members = categoriesCollection.getItemData(option.categoryId).members;
+  for (let i = members.length - 1; i >= 0; i--) {
+    const member = members[i];
     try {
-      await bot.api.sendMessage(member.id, message_);
+      await bot.api.sendMessage(member.id, option.message, {
+        parse_mode: option.markdown ? 'MarkdownV2' : undefined,
+      });
     }
-    catch (err) {
-      const err_ = err as GrammyError;
-      logger.error('sendMessage', 'error_send_message', err_, {chatId: member.id});
+    catch (error_) {
+      const error = error_ as GrammyError;
+      logger.error('sendMessage', 'error_send_message', error, option);
 
       // Remove if blocked
-      if (err_.error_code === 403) {
-        categoryData.memberList = categoryData.memberList.filter((member_) => member_.id !== member.id);
-        categoriesCollection.mergeItemData(categoryId, categoryData);
+      if (error.error_code === 403) {
+        const memberIndex = members.indexOf(member);
+        if (memberIndex === -1) {
+          logger.accident?.('notifyTelegram', 'unexpected_member_not_found_to_remove', {member, members});
+        }
+        members.splice(memberIndex, 1);
         categoriesCollection.save();
       }
     }
   }
+  logger.logStep?.('notify', 'done');
 }
