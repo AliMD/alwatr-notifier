6b0961b91e0a2f0238b3c1346e9406af93abef34
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-10-25 21:56:58 +0330

    feat: requireAccessToken handler

---
+++ packages/notifier-api/src/handler/require-access-token.ts
@@ -0,0 +1,28 @@
+import {HttpStatusCodes, type NanotronClientRequest} from 'alwatr/nanotron';
+
+import {config, logger} from '../config.js';
+import { getAuthBearer } from '../lib/get-auth-bearer.js';
+
+export async function requireAccessToken(this: NanotronClientRequest): Promise<void> {
+  const token = getAuthBearer(this.headers.authorization);
+  logger.logMethodArgs?.('requireAccessToken', {token});
+
+  if (token === null) {
+    this.serverResponse.statusCode = HttpStatusCodes.Error_Client_401_Unauthorized;
+    this.serverResponse.replyErrorResponse({
+      ok: false,
+      errorCode: 'authorization_required',
+      errorMessage: 'Authorization token required'
+    });
+    return;
+  }
+
+  if (token !== config.accessToken) {
+    this.serverResponse.statusCode = HttpStatusCodes.Error_Client_403_Forbidden;
+    this.serverResponse.replyErrorResponse({
+      ok: false,
+      errorCode: 'access_denied',
+      errorMessage: 'Access denied, token is invalid!',
+    });
+  }
+}
