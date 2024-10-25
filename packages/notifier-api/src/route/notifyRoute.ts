09a4a6dfad4567b0a427f77344784e6ede809a94
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-10-25 15:27:36 +0330

    refactor(notifier-api): Update notifyRoute to use notifyValidation and notifyTelegram
    This commit updates the notifyRoute in the notifier-api package. It replaces the previous validation logic with the new notifyValidation handler and updates the message sending logic to use the notifyTelegram function. This improves the overall functionality and maintainability of the code.

--- packages/notifier-api/src/route/notifyRoute.ts
+++ packages/notifier-api/src/route/notifyRoute.ts
@@ -1,80 +1,23 @@
-import {HttpStatusCodes} from 'alwatr/nanotron';
-
-import {config, logger} from '../config.js';
+import {logger} from '../config.js';
+import {notifyValidation} from '../handler/notify-validation.js';
 import {parseBodyAsJson} from '../handler/parse-body-as-json.js';
-import {bot} from '../lib/bot.js';
-import {alwatrNitrobase} from '../lib/nitrobase.js';
+import {notifyTelegram} from '../lib/notifyTelegram.js';
 import {nanotronApiServer} from '../lib/server.js';
 
-import type { GrammyError } from 'grammy';
+import type {NotifyOption} from '../type.js';
 
-nanotronApiServer.defineRoute<{body: JsonObject}>({
+nanotronApiServer.defineRoute<{body: NotifyOption}>({
   method: 'POST',
   url: 'notify',
-  preHandlers: [parseBodyAsJson],
+  preHandlers: [parseBodyAsJson, notifyValidation],
   async handler() {
-    logger.logMethod?.('notify');
+    logger.logMethod?.('notifyRoute');
 
-    const body = this.sharedMeta.body;
-
-    const categoryId = body.category;
-    if (categoryId === undefined || typeof categoryId !== 'string') {
-      this.serverResponse.statusCode = HttpStatusCodes.Error_Client_400_Bad_Request;
-      this.serverResponse.replyJson({
-        ok: false,
-        errorCode: 'category_required',
-        errorMessage: 'Category is required.',
-      });
-      return;
-    }
-
-    const message = body.message;
-    if (message === undefined || typeof message !== 'string') {
-      this.serverResponse.statusCode = HttpStatusCodes.Error_Client_400_Bad_Request;
-      this.serverResponse.replyJson({
-        ok: false,
-        errorCode: 'message_required',
-        errorMessage: 'Message is required.',
-      });
-      return;
-    }
-
-    const categoriesCollection = await alwatrNitrobase.openCollection<Category>(config.nitrobase.categoriesCollection);
-
-    if (categoriesCollection.hasItem(categoryId) === false) {
-      this.serverResponse.statusCode = HttpStatusCodes.Error_Client_400_Bad_Request;
-      this.serverResponse.replyJson({
-        ok: false,
-        errorCode: 'category_not_found',
-        errorMessage: 'Category not found.',
-      });
-      return;
-    }
+    const notifyOption = this.sharedMeta.body;
+    notifyTelegram(notifyOption); // no need to await
 
     this.serverResponse.replyJson({
       ok: true,
     });
-
-    const members = categoriesCollection.getItemData(categoryId).members;
-    for (let i = members.length - 1; i >= 0; i--) {
-      const member = members[i];
-      try {
-        await bot.api.sendMessage(member.id, message, {
-          parse_mode: body.markdown === true ? 'MarkdownV2' : undefined,
-        });
-      }
-      catch (error_) {
-        const error = error_ as GrammyError;
-        logger.error('sendMessage', 'error_send_message', error, {member, categoryId, message});
-
-        // Remove if blocked
-        if (error.error_code === 403) {
-          const memberIndex = members.findIndex((member_) => member_ === member);
-          members.splice(memberIndex, 1);
-          categoriesCollection.save();
-        }
-      }
-    }
   },
 });
-
